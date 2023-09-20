bf85cf2ca250e68f026a7f84aac587db204950e1
Vlad Duplin <vlad.duplin@ya.ru>
2023-09-20 18:16:29 +0300

    Group imports

--- label/main_test.go
+++ label/main_test.go
@@ -1,50 +1,51 @@
 package label
 
 import (
-	"github.com/stretchr/testify/require"
-	"testing"
+    "testing"
+
+    "github.com/stretchr/testify/require"
 )
 
 func TestLabel(t *testing.T) {
-	testCases := []struct {
-		name     string
-		str      string
-		labels   []Label
-		expected string
-	}{
-		{
-			"one",
-			"somesuperlongstring",
-			[]Label{
-				{1, 2, "la"},
-				{4, 4, "d"},
-				{10, 13, "tiny"},
-			},
-			"lamdsupertinystring",
-		},
-		{
-			"two",
-			"somesuperlongstring",
-			[]Label{
-				{1, 2, "la"},
-				{4, 4, "d"},
-				{10, 13, "tiny"},
-				{4, 5, "ed"},
-			},
-			"lamedupertinystring",
-		},
-		{
-			"no labels",
-			"somesuperlongstring",
-			[]Label{},
-			"somesuperlongstring",
-		},
-	}
+    testCases := []struct {
+        name     string
+        str      string
+        labels   []Label
+        expected string
+    }{
+        {
+            "one",
+            "somesuperlongstring",
+            []Label{
+                {1, 2, "la"},
+                {4, 4, "d"},
+                {10, 13, "tiny"},
+            },
+            "lamdsupertinystring",
+        },
+        {
+            "two",
+            "somesuperlongstring",
+            []Label{
+                {1, 2, "la"},
+                {4, 4, "d"},
+                {10, 13, "tiny"},
+                {4, 5, "ed"},
+            },
+            "lamedupertinystring",
+        },
+        {
+            "no labels",
+            "somesuperlongstring",
+            []Label{},
+            "somesuperlongstring",
+        },
+    }
 
-	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T) {
-			result := label(tc.str, tc.labels)
-			require.Equal(t, tc.expected, result)
-		})
-	}
+    for _, tc := range testCases {
+        t.Run(tc.name, func(t *testing.T) {
+            result := label(tc.str, tc.labels)
+            require.Equal(t, tc.expected, result)
+        })
+    }
 }
