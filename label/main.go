7c8cc8001f0b629834baab0e4f8a784af81bc133
Vlad Duplin <vlad.duplin@ya.ru>
2023-09-20 12:36:07 +0300

    Add tests

---
+++ label/main.go
@@ -0,0 +1,15 @@
+package main
+
+type Label struct {
+	start int
+	end   int
+	s     string
+}
+
+func main() {
+	// somesuperlongstring
+}
+
+func label(str string, labels []Label) string {
+	return "lamdsupertinystring"
+}
---
+++ label/main_test.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestLabel(t *testing.T) {
+	testCases := []struct {
+		name     string
+		str      string
+		labels   []Label
+		expected string
+	}{
+		{
+			"one",
+			"somesuperlongstring",
+			[]Label{
+				{1, 2, "la"},
+				{4, 4, "d"},
+				{10, 13, "tiny"},
+			},
+			"lamdsupertinystring",
+		},
+		{
+			"two",
+			"somesuperlongstring",
+			[]Label{
+				{1, 2, "la"},
+				{4, 4, "d"},
+				{10, 13, "tiny"},
+				{4, 5, "ed"},
+			},
+			"lamedupertinystring",
+		},
+		{
+			"no labels",
+			"somesuperlongstring",
+			[]Label{},
+			"somesuperlongstring",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			result := label(tc.str, tc.labels)
+			require.Equal(t, tc.expected, result)
+		})
+	}
+}
